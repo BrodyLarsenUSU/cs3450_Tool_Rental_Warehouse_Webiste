a5e6b081809fbbe799975c5cb1c12ad34ccd3ff4
Christian Winwood <christian.winwood@aggiemail.usu.edu>
2020-02-18 14:22:47 -0700

    Worked on Customer Object

--- Spring/src/main/java/com/softwareEngineering/Spring/Models/Customer.java
+++ Spring/src/main/java/com/softwareEngineering/Spring/Models/Customer.java
@@ -5,14 +5,84 @@ import org.springframework.data.annotation.Id;
 public class Customer{
 		@Id 
 		public String id;
-		public String name;
+
+		private String firstName;
+		private String lastName;
+		private int age;
+		private boolean active;
+		private boolean employee;
+		private String gender;
+
 
 		Customer(){
 
 		}
 
-		Customer(String n){
-			name = n;
+		Customer(String fn, String ln, int ag, boolean ac, boolean e, String g){
+			firstName = fn;
+			lastName = ln;
+			age = ag;
+			active = ac;
+			employee = e;
+			gender = g;
 		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public boolean isActive() {
+			return active;
+		}
+
+		public void setActive(boolean active) {
+			this.active = active;
+		}
+
+		public boolean isEmployee() {
+			return employee;
+		}
+
+		public void setEmployee(boolean employee) {
+			this.employee = employee;
+		}
+
+		public String getGender() {
+			return gender;
+		}
+
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+
+		
 
 }
--- Spring/src/main/java/com/softwareEngineering/Spring/Repositories/CustomerRepository.java
+++ Spring/src/main/java/com/softwareEngineering/Spring/Repositories/CustomerRepository.java
@@ -1,10 +1,9 @@
 package com.softwareEngineering.Spring.Repositories;
 
-import java.util.List;
 import com.softwareEngineering.Spring.Models.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CustomerRepository extends MongoRepository<Customer, String>{
-	public Customer findCustomerByName(String name);
+	public Customer findCustomerByFirstName(String firstName);
 	public Customer findCustomerById(int id);
 }
