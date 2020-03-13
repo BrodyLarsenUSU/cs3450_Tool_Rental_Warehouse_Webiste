f7984050d1f2084d9bf0803e3f3c3175fba5b82e
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-03-12 22:19:10 -0600

    Sign Up is working

---
+++ Spring/src/main/java/com/softwareEngineering/Spring/Controllers/CustomerController.java
@@ -0,0 +1,45 @@
+package com.softwareEngineering.Spring.Controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import com.softwareEngineering.Spring.Models.*;
+import com.softwareEngineering.Spring.Models.DTOs.customerDto;
+import com.softwareEngineering.Spring.Repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class CustomerController{
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@GetMapping("/customer")
+	public String getDB(Model model){
+		// findCustomerByName();
+		//model.addAttribute("customer", new Customer());
+		return "index";
+	}
+
+	@PostMapping("/customer")
+	public String saveCustomer(@ModelAttribute("customer") @Valid customerDto customerDto){
+		Customer registered = new Customer();
+		registered.setFirstName(customerDto.getFirstName());
+		registered.setLastName(customerDto.getLastName());
+		registered.setAge(customerDto.getAge());
+		registered.setGender(customerDto.getGender());
+		registered.setPassword(customerDto.getPassword());
+		customerRepository.save(registered);
+		return "redirect:/index";
+	}
+}
--- Spring/src/main/java/com/softwareEngineering/Spring/Controllers/WelcomeController.java
+++ Spring/src/main/java/com/softwareEngineering/Spring/Controllers/WelcomeController.java
@@ -1,17 +1,28 @@
-package com.softwareEngineering.Spring;
+package com.softwareEngineering.Spring.Controllers;
 
 import com.softwareEngineering.Spring.Repositories.ToolRepository;
-
+import com.softwareEngineering.Spring.Repositories.CustomerRepository;
+import com.softwareEngineering.Spring.Models.DTOs.customerDto;
+import com.softwareEngineering.Spring.Models.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WelcomeController {
 
     @Autowired
     private ToolRepository toolRepo;
+	@Autowired
+	private CustomerRepository customerRepo;
+
+	@RequestMapping("/")
+    public String getStarterIndex(){
+        return "index";
+    }
 
     @RequestMapping("/index")
     public String getIndex(){
@@ -35,8 +46,9 @@ public class WelcomeController {
     }
 
     @RequestMapping("/signup")
-    public String getSignUP(){
+    public String getSignUP(Model model){
+        customerDto custDto = new customerDto();
+		model.addAttribute("customer", custDto);
         return "signUp";
     }
-
 }
