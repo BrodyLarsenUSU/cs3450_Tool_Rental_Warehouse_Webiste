80968af3016c8a92601b797500c673efaf664e1b
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-04-06 21:29:34 -0600

    Removed unnecessary import

--- Spring/src/main/java/com/softwareEngineering/Spring/Application.java
+++ Spring/src/main/java/com/softwareEngineering/Spring/Application.java
@@ -1,7 +1,5 @@
 package com.softwareEngineering.Spring;
 
-import com.softwareEngineering.Spring.Models.Customer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
