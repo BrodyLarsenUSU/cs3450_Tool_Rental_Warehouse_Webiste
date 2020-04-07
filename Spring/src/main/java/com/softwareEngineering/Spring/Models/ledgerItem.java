78a05e0655817eb100eeef2944fdc883d459f684
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-04-06 21:30:19 -0600

    Tweaked ledger object

--- Spring/src/main/java/com/softwareEngineering/Spring/Models/ledgerItem.java
+++ Spring/src/main/java/com/softwareEngineering/Spring/Models/ledgerItem.java
@@ -1,33 +1,49 @@
 package com.softwareEngineering.Spring.Models;
 
+import com.softwareEngineering.Spring.Repositories.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 
+
 public class ledgerItem {
+
+    @Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private ToolRepository toolRepo;
+	// @Autowired
+	// private LedgerRepository ledgeRepo;
 
 	@Id 
 	public String id;
 
-    private String userId;
-    private String toolId;
+    public Customer user;
+    public Tool tool;
 
     public ledgerItem(){
 
-	}
+    }
+    
+    public ledgerItem(Customer userid, Tool toolid){
+        user = userid;
+        tool = toolid;
+    }
 
 	public String getUserId() {
-		return this.userId;
+		return this.user.id;
 	}
 
-	public void setUserId(String userId) {
-		this.userId = userId;
+	public void setUser(String userId) {
+        user = customerRepository.findCustomerById(userId);
 	}
 
 	public String getToolId() {
-		return this.toolId;
+		return this.tool.id;
 	}
 
-	public void setToolId(String toolId) {
-		this.toolId = toolId;
+	public void setTool(String toolId) {
+        tool = toolRepo.findToolById(toolId);
 	}
 
 }
